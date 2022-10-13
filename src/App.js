f96275d249ce67a35e7f6821fc52f74746e68d35
iamceltic33 <iamceltic9@gmail.com>
2022-10-13 19:44:33 +0600

    App.js cleared

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
+  return <div>
 
-    </div>
-  );
+  </div>
+    ;
 }
 
 export default App;
